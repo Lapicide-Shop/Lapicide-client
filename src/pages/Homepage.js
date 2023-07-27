d7952ba253b7beaec244246c338f745bb5e0012c
Dok <pierredok@outlook.fr>
2023-07-27 17:44:56 +0200

    3- Add fonction Font-size & spacing letter on the project / About page / Homepage / Header and carousel done for mobile

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,42 @@
+import { useState } from "react"
 
 function Homepage(){
+
+    const [text, setText] = useState('LAPICIDE');
+    const [fontSize, setFontSize] = useState(32);
+    const [letterSpacing, setLetterSpacing] = useState(1);
+
+    const handleChangeText = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleChangeFontSize = (e) => {
+        setFontSize(parseInt(e.target.value, 10));
+    };
+
+    const handleChangeLetterSpacing = (e) => {
+        setLetterSpacing(parseInt(e.target.value, 10));
+    };
+
+    const inputStyle = {
+        fontSize: `${fontSize}px`,
+        letterSpacing: `${letterSpacing}px`,
+    };
+
     return(
         <section>
-            <input type="text" className="mt-[60px] laptop:mt-0 outline-none text-center max-w-[90vw]" value="LAPICIDE"/>
-            <div>
-                <div>
+            <div className="inputText" suppressContentEditableWarning={true} contentEditable="true"  onChange={handleChangeText} style={inputStyle}>
+                {text}
+            </div>
+
+            <div className="input-font">
+                <div className="input-fontSize">
                     <p className="mr-">Size</p>
-                    <input type="range" min="8" max="24" step="0.1" className="w-[190px] laptop: h-[1px] bg-black appearance-none outline-none landing-intro-range" />
+                    <input type="range" min="8" max="72" step="0.1" onChange={handleChangeFontSize}  value={fontSize}/>
                 </div>
-                <div>
+                <div className="input-spacingLetter">
                     <p>Spacing</p>
-                    <input type="range" min="8" max="24" step="0.1" className="w-[190px] laptop: h-[1px] bg-black appearance-none outline-none landing-intro-range" />
+                    <input type="range" min="1" max="12" step="0.1" onChange={handleChangeLetterSpacing}  value={letterSpacing} />
                 </div>
 
             </div>
